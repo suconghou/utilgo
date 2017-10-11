c00655e42ac33956e45e7094819fd6dd3d0458bd
suconghou <1126045770@qq.com>
2017-10-11 21:56:40 +0800

    update

--- request.go
+++ request.go
@@ -54,3 +54,38 @@ func PostContentWait(url string, contentType string, body io.Reader) ([]byte, er
 	}
 	return bodyStr, nil
 }
+
+// Dohttp do a http request and return http response
+func Dohttp(url string, method string, reqHeader http.Header, body io.Reader, timeout uint, transport *http.Transport) (*http.Response, error) {
+	client := NewClient(timeout, transport)
+	req, err := NewRequest(url, method, reqHeader, body)
+	if err != nil {
+		return nil, err
+	}
+	return client.Do(req)
+}
+
+// NewRequest return *http.Request
+func NewRequest(url string, method string, reqHeader http.Header, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return req, err
+	}
+	for key, value := range reqHeader {
+		for _, item := range value {
+			req.Header.Set(key, item)
+		}
+	}
+	return req, nil
+}
+
+// NewClient return *http.Client
+func NewClient(timeout uint, transport *http.Transport) *http.Client {
+	var client *http.Client
+	if transport != nil {
+		client = &http.Client{Timeout: time.Duration(timeout) * time.Second, Transport: transport}
+	} else {
+		client = &http.Client{Timeout: time.Duration(timeout) * time.Second}
+	}
+	return client
+}
