00cc18fdca42aa656ef0b201271ab5f9c481b352
suconghou <1126045770@qq.com>
2021-10-13 21:06:25 +0800

    fix: improve

--- util.go
+++ util.go
@@ -346,3 +346,12 @@ func CrossShare(h http.Header, r http.Header, header string) {
 		h.Set("Access-Control-Allow-Headers", header)
 	}
 }
+
+// 反转字符串
+func reverseString(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
+}
